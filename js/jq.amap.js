b58cc43e00a7197c5d8ccef40a6824d0e970eb7e
金英飞 <835317619@qq.com>
2017-05-22 23:11:41 +0800

    jQueryAMap
    基于jQuery封装高德地图API

---
+++ js/jq.amap.js
@@ -0,0 +1,49 @@
+(function( $ ) {
+    'use strict';
+    $.fn.amap = function( options ){
+        var that = this;
+        var SingletonLoadMapResource = (function(){
+            var instantiated = null;
+            return {
+                getInstance: function(){
+                    var interval = window.setInterval(function(){
+                        if( typeof window.initAmap === 'undefined' ){
+                            window.initAmap = 'loading';
+                            $.getScript('http://webapi.amap.com/maps?v=1.3&key=0eb030454d05a6f2bf80394b6245f54e', function(){window.initAmap = true;});
+                        }else if('loading' === window.initAmap){
+                            return false;
+                        }else{
+                            window.clearInterval(interval);
+                            initMap();
+                        }
+                    }, 500);
+                }
+            }
+        })();
+        
+        // 1、如果地图没有初始化，则先进行初始化
+        SingletonLoadMapResource.getInstance();
+        
+        // default options
+        var defaults = {
+            width: '100%',
+            height: '100%',
+            resizeEnable: true,
+            zoom: 10,
+            center: [116.397428, 39.90923], // 北京天安门
+        }
+//      console.info( options );
+        options = $.extend( defaults, options );
+//      console.info( options );
+        
+        /**
+         * 根据ID初始化地图
+         */
+        function initMap(){
+            if(that.length > 1){
+                alert('ID must only one');
+            }
+            new AMap.Map(that.get(0).id, options);
+        }
+    }
+})( jQuery );
---
+++ js/jquery.map.js
@@ -0,0 +1,32 @@
+// 0eb030454d05a6f2bf80394b6245f54e
+/**
+ * jQuery Map Util v1.0.0
+ * @param {Object} factory
+ */
+(function(factory) {
+    'use strict';
+    if(typeof define === 'function' && define.amd) {
+        // AMD register as an anonymous module
+        define(['jquery'], factory);
+    }else if( typeof exports === 'object' ){
+        // Node/CommonJS
+        module.exports = factory(require('jquery'));
+    }else{
+        // Browser globals
+        factory(jQuery);
+    }
+})(function($) {
+    var key = '0eb030454d05a6f2bf80394b6245f54e'; // 地图key秘钥
+    var amap = null;
+    // http://webapi.amap.com/maps?v=1.3&key=0eb030454d05a6f2bf80394b6245f54e&callback=init
+    $.getScript('http://webapi.amap.com/maps?v=1.3&key='+ key, function(){
+        amap = new AMap.Map('J_amap_container', {
+          resizeEable: true,
+          zoom: 10,
+          center: [116.480983, 40.0958]
+        });
+    });
+    
+    
+    
+})
